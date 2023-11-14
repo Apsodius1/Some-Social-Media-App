b4583a3c8907208ad075cf782addb45844d6a93f
Augustin Cibotari <augustus.apso@gmail.com>
2023-11-14 08:49:14 +0200

    Add react-dropzone and implement post creation functionality

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -9,10 +9,22 @@ export const SignupValidation = z.object({
     .min(8, { message: "Password must be at least 8 characters" }),
 });
 
-
 export const SigninValidation = z.object({
   email: z.string().email(),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
+});
+
+export const PostValidation = z.object({
+  caption: z
+    .string()
+    .min(5, { message: "Caption must contain at least 5 characters" })
+    .max(2200),
+  file: z.custom<File[]>(),
+  location: z
+    .string()
+    .min(2, { message: "Location must contain at least 2 characters" })
+    .max(100),
+  tags: z.string(),
 });
